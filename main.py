bf34f027f072c2acae0ccf3a523a5f98bead6999
SHUBHANSHU MISHRA <rollsroyace360@gmail.com>
2023-06-16 20:16:13 +0530

    final app

--- main.py
+++ main.py
@@ -9,7 +9,12 @@ with st.sidebar:
     # collapasable sidebar
     st.header("Instruction")
     with st.expander("How to use", expanded=True):  # hetre instruction will come
-        st.write("hello anshu")
+        st.write("""[1] Enter the name of the product that you want to search for. \n
+[2] Click on the "Search" button. \n
+[3] The system will start to scrape the Amazon website for the product details. \n
+[4] The system may take a few minutes to complete the scraping process.\n
+[5] Once the scraping process is complete, the product details will be displayed on the screen. \n
+[6] If the system does not find any product details for the product that you entered, you will be prompted to start the process again""")
 
 
 # getting input
@@ -26,4 +31,5 @@ if(button_1):
         
         st.write(alfa)
         download_button = st.download_button('Download DataFrame as CSV', data=alfa.to_csv(), file_name='dataframe.csv')
-
+    else:
+        st.write("please enter the product name")
