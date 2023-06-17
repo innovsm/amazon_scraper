ab49825d906f90dc042d82627abc785ef73c6009
SHUBHANSHU MISHRA <rollsroyace360@gmail.com>
2023-06-17 10:34:01 +0530

    ...

--- main.py
+++ main.py
@@ -10,11 +10,12 @@ with st.sidebar:
     st.header("Instruction")
     with st.expander("How to use", expanded=True):  # hetre instruction will come
         st.write("""[1] Enter the name of the product that you want to search for. \n
-[2] Click on the "Search" button. \n
+[2] Click on the "Run" button. \n
 [3] The system will start to scrape the Amazon website for the product details. \n
 [4] The system may take a few minutes to complete the scraping process.\n
 [5] Once the scraping process is complete, the product details will be displayed on the screen. \n
-[6] If the system does not find any product details for the product that you entered, you will be prompted to start the process again""")
+[6] If the system does not find any product details for the product that you entered, you will be prompted to start the process again \n
+[7] If you are not satisfied with the results, you can click on the "Run Again" button to start the process again. \n""")
 
 
 # getting input
